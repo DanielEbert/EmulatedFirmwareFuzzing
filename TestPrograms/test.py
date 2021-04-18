368e4b7f826087e4ea7884735b2a1294e90cad19
Daniel Ebert <iuq@web.de>
2021-04-18 08:30:52 -0700

    tests

--- TestPrograms/test.py
+++ TestPrograms/test.py
@@ -36,14 +36,60 @@ SF not set at pc 65c with origin 64a
 Exiting normally.
 '''
   ),
+  (
+    'uninitialized2', b'''\
+SF not set at pc 7c8 with origin 7c0
+Exiting normally.
+'''
+  ),
+  (
+    'uninitialized', b'''\
+SF not set at pc 7a0 with origin 798
+SF not set at pc 990 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9ae with origin 7b0
+SF not set at pc 9b6 with origin 7b0
+SF not set at pc 9bc with origin 7b0
+SF not set at pc 9c4 with origin 7b0
+Exiting normally.
+'''
+  ),
 ]
 
 for test_dir, required_msg in tests:
   os.chdir(test_dir)
   exit_code = os.system('make --silent')
   assert exit_code == 0, f'make returned exit code {exit_code}'
-  # need to check stderr
   p = subprocess.run(['../../simavr/simavr/run_avr' ,'-m', 'atmega2560', '--run_once_with', 'Makefile', 'src.ino.elf'], stdout = subprocess.PIPE, stderr=subprocess.PIPE, env=test_env)
-  assert required_msg in p.stdout, f'Test {test_dir} FAILED\nstderr for test {test_dir} did not include the required text: {required_msg}'
+  assert required_msg in p.stdout, f'Test {test_dir} FAILED\nstdout for test {test_dir} did not include the required text: {required_msg}\nstdout was:\n{p.stdout}'
   print(f'***** Test {test_dir} SUCCESS')
   os.chdir(cwd)

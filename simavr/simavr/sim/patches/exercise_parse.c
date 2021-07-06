559323836eedeb2328867e6e6dceeaffae796dca
Daniel Ebert <iuq@web.de>
2021-07-06 21:22:52 +0200

    fix bug in patch

--- simavr/simavr/sim/patches/exercise_parse.c
+++ simavr/simavr/sim/patches/exercise_parse.c
@@ -4,7 +4,7 @@
 
 void setup_patches(avr_t *avr) {
   // Uncomment the line below if you want to print the generated inputs to the terminal.
-  // patch_function(get_symbol_address("setup", avr), print_current_input, avr, avr);
+  // patch_function("setup", print_current_input, avr, avr);
   patch_function("setup", write_fuzz_input_global, avr, avr);
   patch_function("UnityConcludeTest", fuzz_reset, avr, avr);
     }

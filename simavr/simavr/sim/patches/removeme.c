c13808c883275d4396e34cd68ba2c911e0639fde
Daniel Ebert <iuq@web.de>
2021-07-06 12:11:54 -0700

    refactor

--- simavr/simavr/sim/patches/removeme.c
+++
@@ -1,8 +0,0 @@
-#include "fuzz_patch_instructions.h"
-#include "sim_avr.h"
-#include <stdio.h>
-
-void setup_patches(avr_t *avr) {
-  patch_instruction(0x6bc, print_current_input, avr);
-  patch_instruction(0x67e, fuzz_reset, avr);
-}
